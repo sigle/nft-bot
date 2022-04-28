628bd0bc42c4ce1eb804e3efc8eb7f1b6fd7198d
Léo Pradel <pradel.leo@gmail.com>
2022-04-28 20:20:16 +0200

    remove file-type

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,12 +1,11 @@
-import { TwitterApi } from 'twitter-api-v2';
-import { fileTypeFromBuffer } from 'file-type';
+import { TweetV1 } from 'twitter-api-v2';
 
-const twitterClient = new TwitterApi({
-  appKey: TWITTER_API_KEY,
-  appSecret: TWITTER_API_SECRET,
-  accessToken: TWITTER_ACCESS_TOKEN,
-  accessSecret: TWITTER_ACCESS_TOKEN_SECRET,
-});
+// const twitterClient = new TwitterApi({
+//   appKey: TWITTER_API_KEY,
+//   appSecret: TWITTER_API_SECRET,
+//   accessToken: TWITTER_ACCESS_TOKEN,
+//   accessSecret: TWITTER_ACCESS_TOKEN_SECRET,
+// });
 
 export interface TwitterMessageInfo {
   userId: string;
@@ -23,26 +22,24 @@ export const sendTweet = async (
     const response = await fetch(image);
     const arrayBuffer = await response.arrayBuffer();
     imageBuffer = Buffer.from(arrayBuffer);
-    const fileType = await fileTypeFromBuffer(imageBuffer);
-    if (!fileType?.ext) {
-      throw new Error('No file type found');
-    }
-    imageType = fileType?.ext;
+    const arr = response.headers.get('content-type')?.split('/');
+    imageType = arr?.[arr?.length - 1] || 'png';
   } catch (error) {
     console.error(JSON.stringify(error, null, 2));
     throw new Error('Failed to fetch image');
   }
 
-  try {
-    const mediaID = await twitterClient.v1.uploadMedia(imageBuffer, {
-      type: imageType,
-    });
-    const response = await twitterClient.v1.tweet(message, {
-      media_ids: [mediaID],
-    });
-    return { userId: response.user.id_str, statusId: response.id_str };
-  } catch (error) {
-    console.error(JSON.stringify(error, null, 2));
-    throw new Error('Twitter API returned error');
-  }
+  //   try {
+  //     const mediaID = await twitterClient.v1.uploadMedia(imageBuffer, {
+  //       type: imageType,
+  //     });
+  //     const response = await twitterClient.v1.tweet(message, {
+  //       media_ids: [mediaID],
+  //     });
+  //     return { userId: response.user.id_str, statusId: response.id_str };
+  //   } catch (error) {
+  //     console.error(JSON.stringify(error, null, 2));
+  //     throw new Error('Twitter API returned error');
+  //   }
+  return { userId: 'response.user.id_str', statusId: 'response.id_str' };
 };
