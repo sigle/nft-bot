282a1796b23e9455e5dcb397826e36325960cbf3
Léo Pradel <pradel.leo@gmail.com>
2022-06-15 11:40:56 +0200

    chore: comment fetch

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import Toucan from 'toucan-js';
 import { handleRequest } from './handler';
 
-addEventListener('fetch', (event) => {
-  const sentry = new Toucan({
-    dsn: SENTRY_DSN,
-    context: event,
-  });
+// addEventListener('fetch', (event) => {
+//   const sentry = new Toucan({
+//     dsn: SENTRY_DSN,
+//     context: event,
+//   });
 
-  event.respondWith(
-    (async function () {
-      try {
-        return await handleRequest();
-      } catch (err) {
-        const sentryId = sentry.captureException(err);
-        return new Response(`Internal server error. Event ID: ${sentryId}`, {
-          status: 500,
-        });
-      }
-    })()
-  );
-});
+//   event.respondWith(
+//     (async function () {
+//       try {
+//         return await handleRequest();
+//       } catch (err) {
+//         const sentryId = sentry.captureException(err);
+//         return new Response(`Internal server error. Event ID: ${sentryId}`, {
+//           status: 500,
+//         });
+//       }
+//     })()
+//   );
+// });
 
 addEventListener('scheduled', (event) => {
   const sentry = new Toucan({
